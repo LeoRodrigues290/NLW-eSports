ed69f8652ed3760cfbe07ae96a16e605896d7080
Léo <leo.rodriguessc@gmail.com>
2022-09-13 13:13:17 -0300

    Testando request com .mjs

---
+++ server/src/server.mjs
@@ -0,0 +1,24 @@
+// ## Forma padrão de importação
+// const express = require('express');
+
+// ## Com nova versão do node .mjs, importação via package.json
+
+import express from 'express'
+
+const app = express()
+
+//www.minhapi.com/ads
+//localhost:3333/ads
+
+//request = buscar informações que estão vindo da requisição (nome, jogo, tempo de jogo,etc.)
+//response = devolver uma resposta
+
+app.get('/ads', (request,response) => {
+    return response.json([
+        {id: 1, name: 'Anúncio 1'},
+        {id: 2, name: 'Anúncio 2'},
+        {id: 3, name: 'Anúncio 3'}
+    ])
+})
+
+app.listen(3333)
