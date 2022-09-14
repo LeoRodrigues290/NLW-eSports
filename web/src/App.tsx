ee1030dad00d1f6d414f5a1e9fe75e221ed28f33
Léo <leo.rodriguessc@gmail.com>
2022-09-14 15:58:56 -0300

    Finished static front web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/main.css';
 import logoimg from './assets/logo.svg';
+import { MagnifyingGlassPlus } from 'phosphor-react'
 
 function App() {
     return (
@@ -68,8 +69,20 @@ function App() {
 
             </div>
 
-            <div className="bg-[#2A2634] px-8 py-6 mt-8 self-stretch rounded-lg">
+            <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8">
+                <div className="bg-[#2A2634] px-8 py-6 rounded-t-lg rounded-b flex justify-between items-center">
 
+                    <div>
+                        <strong className='text-2xl text-white font-black block mb-1'>Não encontrou seu duo?</strong>
+                        <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+                    </div>
+
+                    <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 transition ease-in-out text-white rounded flex items-center gap-3'>
+                        <MagnifyingGlassPlus size={24} />
+                        Publicar anúncio
+                    </button>
+
+                </div>
             </div>
         </div >
     )
