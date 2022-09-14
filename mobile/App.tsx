e7cc4c8fc7121516c893e45e223cabfcbead58d7
Léo <leo.rodriguessc@gmail.com>
2022-09-14 08:27:05 -0300

    Structuring and styling web with Tailwind CSS

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello World!</Text>
+            <Button title={'Send 1'} />
+            <StatusBar style="auto"/>
+        </View>
+    );
+}
+interface ButtonProps{
+    title: string;
+}
+
+function Button(props: ButtonProps){
+    return (
+        <TouchableOpacity>
+            <Text style={styles.button}>
+                {props.title}
+            </Text>
+        </TouchableOpacity>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    title: {
+        color: 'white',
+        fontSize: 22,
+    },
+
+    button: {
+        color: 'purple',
+        backgroundColor: 'white',
+        paddingTop: 20,
+        paddingBottom: 20,
+        paddingLeft: 60,
+        paddingRight: 60,
+    }
 });
