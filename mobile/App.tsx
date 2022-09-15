453ea5f2a3c1d7b4296e8b35873c87635ea2e1ee
Léo <leo.rodriguessc@gmail.com>
2022-09-14 22:35:32 -0300

    Config mobile and update splash

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,48 +1,14 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {StatusBar} from "react-native";
+import {Background} from "./src/componentes/background";
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Hello World!</Text>
-            <Button title={'Send 1'} />
-            <StatusBar style="auto"/>
-        </View>
+        <Background>
+            <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+            />
+        </Background>
     );
 }
-interface ButtonProps{
-    title: string;
-}
-
-function Button(props: ButtonProps){
-    return (
-        <TouchableOpacity>
-            <Text style={styles.button}>
-                {props.title}
-            </Text>
-        </TouchableOpacity>
-    )
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#000',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    title: {
-        color: 'white',
-        fontSize: 22,
-    },
-
-    button: {
-        color: 'purple',
-        backgroundColor: 'white',
-        paddingTop: 20,
-        paddingBottom: 20,
-        paddingLeft: 60,
-        paddingRight: 60,
-    }
-});
