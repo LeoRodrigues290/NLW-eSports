6fc7b2f4a7ff29ff15d18131664ff67aba8d44e9
Léo <leo.rodriguessc@gmail.com>
2022-09-17 13:04:36 -0300

    Font loading integration in mobile app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,26 @@
 import {StatusBar} from "react-native";
 import {Background} from "./src/componentes/background";
+import {Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black, useFonts} from '@expo-google-fonts/inter';
+import {Home} from './src/screens/Home';
+import {Loading} from "./src/componentes/Loading";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        Inter_400Regular,
+        Inter_600SemiBold,
+        Inter_700Bold,
+        Inter_900Black
+    });
+
     return (
         <Background>
             <StatusBar
-            barStyle="light-content"
-            backgroundColor="transparent"
-            translucent
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
             />
+            {fontsLoaded ? <Home/> :  <Loading/>}
+
         </Background>
     );
 }
